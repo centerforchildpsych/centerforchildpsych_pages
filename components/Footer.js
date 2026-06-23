01519ef804556a9bb5198d09b6cc86fa8d99b865
MichaelGalloway404 <michael.d.galloway404@gmail.com>
2026-06-23 09:09:33 -0500

    added simple clean scrolling header, with drop down hamburger. Added simple footer to bottom

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+"use client";
+
+import styles from "./Footer.module.css"
+
+function Footer() {
+    return (
+        <>
+            <p className={styles.easeyStyle}>Copyright 2026 — The Center for Child and Family Psychiatry, PC. All rights reserved.</p>
+        </>
+    );
+}
+
+export default Footer;
---
+++ components/Header.js
@@ -0,0 +1,62 @@
+"use client";
+
+import { useEffect, useState } from 'react';
+import styles from "./Header.module.css"
+
+function Header() {
+    const [stickyHeader, setStickyHeader] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 60) {
+                setStickyHeader(true);
+            } else {
+                setStickyHeader(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    return (
+        <>
+            <p className={styles.easeyStyle}>1103 N. Gray Street | Killeen, TX 76541 | (254) 774-1163 | centerforchildpsych@gmail.com</p>
+
+            <div className={`${styles.scrollngHeader} ${stickyHeader ? styles.sticky : styles.stickyOffset}`}>
+
+                <div>
+                    <img
+                        className={styles.imageLogo}
+                        src='./photos/ChildCenterImg.png'
+                        alt='The Center for Child and Family Psychiatry, PC'
+                    /><p>PHONE: (254) 774-1163 | FAX: 833-464-5455</p>
+                </div>
+
+
+                <div
+                    className={styles.hamburger}
+                    onClick={() => setMenuOpen(!menuOpen)}
+                >
+                    ☰
+                </div>
+
+                <div className={`${styles.navLinks} ${menuOpen ? styles.active : ''}`.trim()}>
+                    <a className={styles.navBarLink} href='#'>Home</a>
+                    <a className={styles.navBarLink} href='#'>About</a>
+                    <a className={styles.navBarLink} href='#'>Accepted Insurances</a>
+                    <a className={styles.navBarLink} href='https://27185.portal.athenahealth.com/'>Patient Portal</a>
+                    <a className={styles.navBarLink} href='https://consumer.scheduling.athena.io/?locationId=27185-1'>BOOK NOW</a>
+                    <a className={styles.navBarLink} href='#'>TMS</a>
+                </div>
+
+            </div>
+        </>
+    );
+}
+
+export default Header;
