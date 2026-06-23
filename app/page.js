ddb4fbe6efbc5fa8d93237861245e85c2f72eb1d
MichaelGalloway404 <michael.d.galloway404@gmail.com>
2026-06-23 11:56:24 -0500

    adding test content to test fit of themes

--- app/page.js
+++ app/page.js
@@ -1,19 +1,39 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+        <img
+          className={styles.imageArc}
+          src="/photos/buildingImage.png"
+          alt="Center for Child Pysch logo"
+          priority="true"
         />
-        <h1><strong>Welcome!</strong></h1>
+
+        <section>
+          <div className={styles.imageTextGrid}>
+            <div className={styles.textSection}>
+              <div className={styles.textItems}>
+                <h3>You wish someone could help you figure this all out…..</h3>
+                <ul>
+                  <li>You’re struggling to communicate in your relationships</li>
+                  <li>Resurfacing problems from your past</li>
+                  <li>Feeling anxious, confused, and lost all the time</li>
+                  <li>And trying to understand who you truly are</li>
+                </ul>
+                <p>To say you’re lacking in direction feels like a big understatement.</p>
+              </div>
+            </div>
+            <div className={styles.imageSection}>
+              <img
+                src="/photos/buildingImage.png"
+                alt="Center for Child Pysch logo"
+                priority="true" />
+            </div>
+          </div>
+        </section>
+
       </main>
     </div>
   );
--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactCompiler: true,
   output: 'export',
+  allowedDevOrigins: ['192.168.1.72', 'localhost'],
 };
 
 export default nextConfig;
